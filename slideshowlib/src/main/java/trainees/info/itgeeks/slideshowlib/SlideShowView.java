097cc31ca992217b47e22765d51d34e256b5a78d
Your Name <you@example.com>
2019-04-12 00:35:36 +0200

    0

---
+++ slideshowlib/src/main/java/trainees/info/itgeeks/slideshowlib/SlideShowView.java
@@ -0,0 +1,13 @@
+package trainees.info.itgeeks.slideshowlib;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.viewpager.widget.ViewPager;
+
+public class SlideShowView extends ViewPager {
+    public SlideShowView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
