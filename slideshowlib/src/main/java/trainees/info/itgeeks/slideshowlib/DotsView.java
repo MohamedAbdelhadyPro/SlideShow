7a90b6992736d9c58b7b74fc3421bb0c1cd0663c
Your Name <you@example.com>
2019-04-12 02:47:58 +0200

    v1

--- slideshowlib/src/main/java/trainees/info/itgeeks/slideshowlib/DotsView.java
+++ slideshowlib/src/main/java/trainees/info/itgeeks/slideshowlib/DotsView.java
@@ -4,13 +4,10 @@ import android.content.Context;
 import android.util.AttributeSet;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.viewpager.widget.ViewPager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class SlideShowView extends ViewPager {
-
-    public SlideShowView(@NonNull Context context, @Nullable AttributeSet attrs) {
+public class DotsView extends RecyclerView {
+    public DotsView(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-
     }
-
 }
